edec10b60f6b5189fd5ac4eea3f273e0e4ee8506
Josef Kořínek <josefkorinek@bezobsahu.cz>
2022-01-27 18:14:14 +0100

    qgis plugin create

---
+++ MasterThesis/DP_podklady/DP_podklady/SecondScriptTesting.py
@@ -0,0 +1,17 @@
+import os
+import sys
+
+firsFileName="Bio_chmel_200427"
+
+absolutepath = os.path.abspath('SecondScriptTesting.py')
+print(absolutepath)
+
+fileDirectory = os.path.dirname(absolutepath)
+print(fileDirectory)
+#Path of parent directory
+parentDirectory = os.path.dirname(fileDirectory)
+print(parentDirectory)
+#Navigate to Strings directory
+newPath = os.path.join(parentDirectory, 'Strings')   
+print(newPath)
+
